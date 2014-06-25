00773f8f921d043503c07d9f4ebb92f94ae84316
hugo-pH <hugoycia@gmail.com>
2014-06-25 12:03:08 +0200

    add homegeneity folder

---
+++ homogeneity/README.md
@@ -0,0 +1,10 @@
+#Homocedasticity analysis
+
+This shiny app performs a graphic and statistical analyisis of the data homogenity of variance over a dataset selected by the user. 
+
+The data is retrieved from a Chado DB and ploted in an Boxplot. In addition three statistical tests for homocedasticity are applied, Bartlett test, Levene's test and Fligner-Killen test of homogenity of varainces. 
+
+An example of one of the plot outputs:
+![Alt text](../images/.png?raw=true)
+
+You can take a look here: http://www.gefpdb.net/variance_analysis
---
+++ homogeneity/server.R
@@ -0,0 +1,123 @@
+#server.R, eval_disp
+#Load libraries for shiny, plots and PgSQL connection
+library(shiny)
+library(ggplot2)
+library("RPostgreSQL")
+library(agricolae)
+library(car)
+library(data.table)
+library(plyr)
+#Connect to the database
+drv<-dbDriver("PostgreSQL")
+con <- dbConnect(drv, dbname="drupal", host="10.0.0.16", user="drupal", port ="5432")
+#Set the search path to chado, public in the database
+dbSendQuery(con, "SET search_path TO chado, public;")
+#Query the DB to get all the phenotypics values from all attributes and store it in a dataframe
+bulkdata<- dbGetQuery(con, "SELECT s2.uniquename AS stock, esp.value AS season,
+                      c1.name AS attribute, p.value AS value, epr.value AS date, cvp.value AS unit 
+                      FROM stock s 
+                      JOIN stock_relationship sr ON sr.subject_id = s.stock_id
+                      JOIN stock s2 ON sr.object_id=s2.stock_id
+                      JOIN nd_experiment_stock es ON s.stock_id = es.stock_id
+                      JOIN nd_experiment_stockprop esp ON es.nd_experiment_stock_id = esp.nd_experiment_stock_id         
+                      LEFT JOIN chado_stock ck ON s.stock_id = ck.stock_id         
+                      JOIN nd_experiment_phenotype ep ON es.nd_experiment_id = ep.nd_experiment_id             
+                      JOIN nd_experimentprop epr ON ep.nd_experiment_id = epr.nd_experiment_id            
+                      JOIN phenotype p ON ep.phenotype_id = p.phenotype_id          
+                      JOIN cvterm c1 ON p.attr_id = c1.cvterm_id            
+                      JOIN cvtermprop cvp ON cvp.cvterm_id = c1.cvterm_id
+                      WHERE cvp.type_id = 43425")
+bulkdata$value<-as.numeric(bulkdata$value)
+postgresqlCloseConnection(con)
+
+shinyServer(function(input, output) {
+  
+  #Reactive object to subset the bulkdata depending on the attribute input. It will be used to determine which seasons are available for a given attribute.
+  options<-reactive({
+    subset(bulkdata, attribute==input$attribute)
+  })
+  #Create the selectInput for seasons. 
+  output$select.season<-renderUI({
+    if (is.null(input$attribute) == TRUE){ # If no attribute is selected, return a empty object.
+    }else{
+      return()
+    }else{
+      options<-options()
+      seas<-as.vector(options[["season"]])
+      
+      selectInput("season",
+                  label = "Choose a season",
+                  choices = seas,
+                  selected = NULL)
+    }
+    
+  })
+  
+  #Get the dataset which will be ploted latter, subset bulkdata with the selected inputs, attribute and season
+  data<-reactive({
+    if (input$go==0){# if the 'Run' button is not clicked, return nothing.
+      return()
+    }else{
+      attribute <- input$attribute  
+      season<-input$season
+      subset(bulkdata, attribute==input$attribute & season==input$season)
+    }
+  })
+  #############
+  ###Boxplot###
+  #############
+  output$map<-renderPlot({
+    if (input$go==0){ # if the 'Run' button is not clicked, return nothing.
+      return()
+    }else{
+      data<-data()
+      p<-ggplot(data(), aes(factor(stock), value)) +
+        labs(x="Stock", y = paste0(input$attribute, " ", "(", unique(data$unit), ")")) +
+        scale_fill_discrete(guide=FALSE) + #No legend
+        theme(axis.text.x=element_text(face="bold", angle=60, hjust=1,  size=12))#Turn 60º the labels of x axis
+      
+      p <- p  +  geom_boxplot(aes(fill = factor(stock))) 
+      print(p)
+    } 
+  })
+  
+  ###################
+  ###Bartlett test###
+  ###################
+  output$bart<- renderPrint({
+    if (input$go==0){ # if the 'Run' button is not clicked, return nothing.
+      return()
+    }else{
+    data<-data()
+    bart.res<-bartlett.test(value ~ stock, data=data)
+    bart.res
+    }
+  })
+  
+  #################
+  ###Levene test###
+  #################
+  output$lev<- renderPrint({
+    if (input$go==0){ # if the 'Run' button is not clicked, return nothing.
+      return(NULL)
+    }else{
+    data<-data()
+    lev.res<-leveneTest(value ~ stock, data=data)
+    lev.res
+    }
+  })
+  
+  ##################
+  ###Fligner test###
+  ##################
+  output$flig<- renderPrint({
+    if (input$go==0){ # if the 'Run' button is not clicked, return nothing.
+      return(NULL)
+    }else{
+    data<-data()
+    flig.res<-fligner.test(data$value, as.factor(data$stock))
+    flig.res
+    }
+  })
+  
+})
